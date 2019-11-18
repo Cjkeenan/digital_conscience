61390e0294416124923441477a47ca640457ac03
cjkeenan <cjkeenan@live.com>
2019-11-17 18:08:57 -0800

    Initial Commit - GUI programmed and logic working, need to add dictionary of bad phrases

---
+++ Digital_Conscience/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Digital_Conscience
+{
+    partial class InitialWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clearButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.messageText = new System.Windows.Forms.RichTextBox();
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.clearButton.Location = new System.Drawing.Point(37, 111);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 25);
+            this.clearButton.TabIndex = 0;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendButton.Location = new System.Drawing.Point(216, 98);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(251, 38);
+            this.sendButton.TabIndex = 1;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // messageText
+            // 
+            this.messageText.AcceptsTab = true;
+            this.messageText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageText.Location = new System.Drawing.Point(22, 15);
+            this.messageText.Name = "messageText";
+            this.messageText.ShowSelectionMargin = true;
+            this.messageText.Size = new System.Drawing.Size(455, 77);
+            this.messageText.TabIndex = 3;
+            this.messageText.Text = "Enter your message here...";
+            this.messageText.UseWaitCursor = true;
+            // 
+            // InitialWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(499, 149);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.clearButton);
+            this.Name = "InitialWindow";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.RichTextBox messageText;
+    }
+}
+
---
+++ Digital_Conscience/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Conscience
+{
+    public partial class InitialWindow : Form
+    {
+        public InitialWindow()
+        {
+            InitializeComponent();
+            if (messageText.Focused)
+                messageText.SelectAll();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            messageText.Clear();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            bool conscienceFlag = ScanText(messageText.Text);
+            if (conscienceFlag)
+            {
+                TabooDialog();
+            }
+            
+        }
+
+        private bool ScanText(string text)
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '\n', ';'};
+            string[] textArray = text.Split(delimiterChars);
+            List<string> tabooWords = PullTabooWords();
+
+            bool flag = false;         
+
+            foreach(string taboo in tabooWords)
+            {
+                foreach(string word in textArray)
+                {
+                    if (word.Contains(taboo, StringComparison.OrdinalIgnoreCase))
+                        flag = true;
+                }
+            }
+            return flag;
+        }
+
+        public void TabooDialog()
+        {
+            var tabooDlg = MessageBox.Show("Your message contained words that are common with those that are considered hostile or hateful.\nAre you sure you want to post this ?", "Warning Message", MessageBoxButtons.YesNo);
+            if (tabooDlg == System.Windows.Forms.DialogResult.Yes)
+            {
+                var messageStatusDlg = MessageBox.Show("Message Sent Succesfully","Message Status", MessageBoxButtons.OK);
+                this.Close();
+            }
+        }
+
+        private List<string> PullTabooWords()
+        {
+            List<string> tabooWords = new List<string>();
+            tabooWords.Add("hello");
+            tabooWords.Add("message");
+            return tabooWords;
+        }
+    }
+}
